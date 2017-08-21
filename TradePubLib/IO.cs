4c34130b53be18b49f1d3537470ebecb8f489273
lokizoo <132756@qq.com>
2017-08-21 15:52:45 +0800

    1

---
+++ TradePubLib/IO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevelopLibrary.DevelopAPI;
+
+namespace TradePubLib
+{
+    public class IO : IndicatorBase
+    {
+        public enum ENU_msgType 
+        {
+            msg_Error = 1,
+            msg_Info = 2
+        }
+         
+        /*
+        public static void PrintMemo(string msg, DateTime time, ENU_msgType msgType = ENU_msgType.msg_Info, bool bPrintTime = true)
+        {
+            string sType = "";            
+            string msgTime = TickNow.ToString("HH:mm:ss");
+
+            switch (msgType)
+            {
+                case ENU_msgType.msg_Info:
+                    sType = "[I][" + SYMBOL + "]";
+                    break;
+                case ENU_msgType.msg_Error:
+                    sType = "[R][" + SYMBOL + "]";
+                    break;
+                default:
+                    sType = "[U][" + SYMBOL + "]";
+                    break;
+            }
+
+            if (bPrintTime)
+            {
+                PrintLine(sType + msgTime + ": " + msg);
+            }
+            else
+            {
+                PrintLine(sType + msg);
+            }
+        } 
+         * */
+
+    }
+}
